e791aca0dc63ddc427b817b0704044a34565ce54
Ssekli <111066892+Ssekli@users.noreply.github.com>
2022-08-23 14:36:53 +0200

    Add files via upload

--- main timer.py
+++ main timer.py
@@ -3,20 +3,15 @@ import time
 
 
 def cooking_choices() :
-    cook = False
-    while cook == False :
-        cook = input("Quelle cuisson voulez vous ? Tapez 1 pour à la coque, 2 pour mollets et 3 pour durs")
-        if cook == 1 :
+    duration = 0
+    while duration == 0 :
+        cook = input("Quelle cuisson voulez vous ? (coque, mollets ou durs)")
+        if cook == "coque" :
             duration = 3
-            cook = True
-        elif cook == 2 :
+        elif cook == "mollets" :
             duration = 6
-            cook = True
-        elif cook == 3 :
+        elif cook == "durs" :
             duration = 9
-            cook = True
-        else :
-            print("Erreur, entrez 1, 2 ou 3 selon votre cuisson(respectivement coque, mollets et durs")
     
     cooking_time = duration * 60
     return cooking_time
@@ -24,20 +19,28 @@ def cooking_choices() :
 
 def timer(cooking_time) :
     while cooking_time :
-        for i in range(10) :
-            time.sleep(1)
-            print(".", end="", flush=True)
+        
         minutes = cooking_time//60
         secondes = cooking_time-minutes*60
         cooking_time -= 10
         print(f"{minutes:02d}:{secondes:02d}")
+        for i in range(10) :
+            time.sleep(1)
+            print(".", end="", flush=True)
+        
+        
 
-    
-        
-        
     print("Cuisson terminée")
             
             
 
 cooking_time = cooking_choices()
 timer(cooking_time)
+
+
+
+
+
+    
+
+
