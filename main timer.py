f9e853dd2f90f5c3bbebbbc413eac8574635f918
Ssekli <111066892+Ssekli@users.noreply.github.com>
2022-08-23 14:22:03 +0200

    Add files via upload

--- main timer.py
+++ main timer.py
@@ -3,14 +3,20 @@ import time
 
 
 def cooking_choices() :
-    duration = 0
-    cook = input("Quelle cuisson voulez vous ? (coque, mollets ou durs)")
-    if cook == "coque" :
-        duration = 3
-    elif cook == "mollets" :
-        duration = 6
-    elif cook == "durs" :
-        duration = 9
+    cook = False
+    while cook == False :
+        cook = input("Quelle cuisson voulez vous ? Tapez 1 pour à la coque, 2 pour mollets et 3 pour durs")
+        if cook == 1 :
+            duration = 3
+            cook = True
+        elif cook == 2 :
+            duration = 6
+            cook = True
+        elif cook == 3 :
+            duration = 9
+            cook = True
+        else :
+            print("Erreur, entrez 1, 2 ou 3 selon votre cuisson(respectivement coque, mollets et durs")
     
     cooking_time = duration * 60
     return cooking_time
@@ -18,24 +24,20 @@ def cooking_choices() :
 
 def timer(cooking_time) :
     while cooking_time :
-        minutes = cooking_time//60
-        secondes = cooking_time-minutes*60
-        min_sec_format = "{:02d}:{02d}". format(minutes, secondes)
         for i in range(10) :
             time.sleep(1)
-            print(min_sec_format, end="", flush=True)
-        print(min_sec_format, end='/r')
+            print(".", end="", flush=True)
+        minutes = cooking_time//60
+        secondes = cooking_time-minutes*60
+        cooking_time -= 10
+        print(f"{minutes:02d}:{secondes:02d}")
+
+    
+        
+        
     print("Cuisson terminée")
             
             
 
 cooking_time = cooking_choices()
 timer(cooking_time)
-
-
-
-
-
-    
-
-
