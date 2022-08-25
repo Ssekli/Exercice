b1e4c14a5d894a016f2a01e6b6c5da69ffd187f5
Ssekli <111066892+Ssekli@users.noreply.github.com>
2022-08-25 15:50:14 +0200

    Add files via upload

--- mainquizz.py
+++ mainquizz.py
@@ -26,17 +26,27 @@ else :
     print("Mauvaise réponse")'''
 
 
-def ask_questions(question, a1, a2, a3, a4, right_answer) :
+def ask_questions(question, a1, a2, a3, a4, right_choice) :
+    global score
     print("Question :", question)
     print("a :", a1)
     print("b :", a2)
     print("c :", a3)
     print("d :", a4)
     answer = input("Votre réponse :")
-    if answer == right_answer :
+    right_answer = False
+    if answer == right_choice :
         print("Bonne réponse")
+        score += 1
     else :
         print("Mauvaise réponse")
 
+    print() #reachable car pas de return avt
+    
+
+
+score = 0 
 ask_questions("Quelle est la capitale de la France ? ","Marseille", "Lille","Paris", "Dunkerque", "c" )
 ask_questions("Quelle est la capitale de l'Italie' ? ", "Vienne", "Rome", "Turin", "Dunkerque", "b")
+
+print("Votre score est:", score)
