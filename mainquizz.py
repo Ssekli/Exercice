a4031d1727c1495768dd90f58d343fd1e68d3f45
Ssekli <111066892+Ssekli@users.noreply.github.com>
2022-08-25 14:38:45 +0200

    Add files via upload

---
+++ mainquizz.py
@@ -0,0 +1,67 @@
+def Q1() :
+    print("Quelle est la capitale de la France ? ")
+    print("a : Marseille")
+    print("b : Lille")
+    print("c : Paris")
+    print("d : Dunkerque")
+    answer = input("Votre réponse :")
+    if answer == "c" :
+        True
+        print("Bonne réponse")
+    else :
+        False
+        print("Mauvaise réponse")
+
+def Q2() :
+    print("Quelle est la capitale de l'Italie' ? ")
+    print("a : Vienne")
+    print("b : Rome")
+    print("c : Turin")
+    print("d : Dunkerque")
+    answer = input("Votre réponse :")
+    if answer == "b" :
+        True
+        print("Bonne réponse")
+    else :
+        False
+        print("Mauvaise réponse")
+
+
+def Q3() :
+    print("Quelle est la devise américaine  ? ")
+    print("a : Dollar")
+    print("b : Euro")
+    print("c : Livre")
+    print("d : Deutschmark")
+    answer = input("Votre réponse :")
+    if answer == "a" :
+        True
+        print("Bonne réponse")
+    else :
+        False
+        print("Mauvaise réponse")
+
+def Q4() :
+    print("Que signifie H2O ? ")
+    print("a : Eau")
+    print("b : Pastis")
+    print("c : Acide chloridrique")
+    print("d : Dunkerque")
+    answer = input("Votre réponse :")
+    if answer == "c" :
+        True
+        print("Bonne réponse")
+    else :
+        False
+        print("Mauvaise réponse")
+
+nb_questions = 0
+
+while nb_questions == 0 :
+    str_nb_questions = input("Combien de questions voulez-vous ?")
+    try : 
+        str_nb_questions = int(nb_questions)
+    except ValueError:
+
+def Quizz(nb_questions):
+    
