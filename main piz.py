fcd6c3c915ba5aefd1dfb4fda91d0af7a191da84
Ssekli <111066892+Ssekli@users.noreply.github.com>
2022-10-18 16:37:12 +0200

    Add files via upload

--- main piz.py
+++ main piz.py
@@ -1,5 +1,13 @@
-#pizzas = ("4 fromages", "picante", "tuna", "reine")
-pizzas =()
+pizzas = ["4 fromages", "picante", "tuna", "reine"]
+#pizzas =()
+
+def add_piz() :
+    new_pizz = input("ajouter une pizza : ") 
+    if new_pizz.lower() in pizzas :
+        print("la pizza existe déjà")
+    else :
+        pizzas.append(new_pizz)
+
 def show_piz() :
     if not pizzas :
         print ("No Pizzas")
@@ -11,4 +19,6 @@ def show_piz() :
         print("1st pizza", pizzas[0])
         print("last pizza", pizzas[-1])
 
+show_piz()
+add_piz()
 show_piz()
